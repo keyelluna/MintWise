4188075f73cec210ebc6013b5a401755d34506a2
keyelluna <lunakimlouren.a@gmail.com>
2025-12-25 22:48:46 +0800

    change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Pool } = require('pg');
-const session = require('express-session');
+// const session = require('express-session');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
